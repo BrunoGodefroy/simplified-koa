1bcef85619ff5264a625b95edac9bd4dc3abd148
BrunoGodefroy <brunog@theodo.co.uk>
2017-08-13 14:18:05 +0100

    Returning a new clean middleware context for each request

--- koa.js
+++ koa.js
@@ -1,49 +1,47 @@
 const http = require('http')
 const co = require('co')
 
-const app = {
+const Koa = () => ({
   middleware: [],
-  getGeneratorsContext: (req, res) => { body: '', req, res },
+  getGeneratorsContext: (req, res) => ({ body: '', req, res }),
   use: function (gen) { this.middleware.push(gen) },
   listen: function (port) { http.createServer(this.callback(this)).listen(port) },
   callback: ctx => (req, res) => {
-    ctx.genCtx.req = req
-    ctx.genCtx.res = res
-    co(ctx.compose(ctx.middleware))
-      .then(() => res.end(ctx.genCtx.body))
+    let generatorsContext = ctx.getGeneratorsContext(req, res)
+    co(ctx.compose(ctx.middleware, generatorsContext))
+      .then(() => res.end(generatorsContext.body))
       .catch(error => console.error(error.stack))
   },
-  compose: function (middleware) {
+  compose: function (middleware, generatorsContext) {
     ctx = this
     const dispatch = i => {
       return function* () {
         if (i >= middleware.length) return
         const next = () => co(dispatch(i + 1))
-        yield middleware[i].call(ctx.genCtx, next)
+        yield middleware[i].call(generatorsContext, next)
       }
     }
     return dispatch(0)
   },
-}
+})
 
 
-
+const app = Koa()
 
 // logger
 
 app.use(function* (next) {
   const start = new Date
-  console.log('Starting: ', this.body)
+  console.log('recieving request: %s %s', this.req.method, this.req.url)
   yield next()
-  console.log('End: ', this.body)
   const ms = new Date - start
-  console.log('%s', ms)
+  console.log('Request time: %d ms', ms)
 });
 
 // response
 
 app.use(function* () {
-  console.log('setting up body')
+  console.log('Setting up body...')
   this.body = 'Hello World'
 });
 
